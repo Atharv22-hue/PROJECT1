a55b38347a12170f24592f1e1fe77401cb5096d1
Atharv22-hue <159293881+Atharv22-hue@users.noreply.github.com>
2024-08-24 17:13:30 +0530

    TODO APP
    this is a todo app made by me ,in which we can writes the works we have to do in future .It is fully safe as it has sign in login section for personnel safety.

--- app.js
+++ app.js
@@ -1,86 +1,58 @@
 const express=require('express');
 const expressLayout=require('express-ejs-layouts');
-
-// cookie parser
-
-
-
-
-const mongoose =require( 'mongoose');
-
+const bodyParser=require('body-parser');
+const methodOverride=require('method-override');
+const mongoose=require('mongoose');
+const {isActive}=require('../to do 2/back/helpers/help');
 const app=express();
-
-
-app.use(express.static('public'));
-
-app.use(expressLayout);
-
-
-// for login 
-const bodyParser = require('body-parser');
-
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-
-// for register the things
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+app.locals.isActive=isActive;
 const cookieparser=require('cookie-parser');
-const mongo=require('connect-mongo');
 const session=require('express-session');
+const MongoStore = require('connect-mongo');
 
-
+const mongoUrl='mongodb+srv://pant2:awapx7UCOhQn4GBa@atharv.09g0rli.mongodb.net/';
 
 app.use(cookieparser());
-app.set('layout','./layouts/main.ejs');
 
+
+app.use(session({
+  secret:'keyboard cat',
+  resave:false,
+  saveUninitialized:true,
+  store:MongoStore.create({
+    mongoUrl:mongoUrl
+  })
+}))
+
+const path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
 
 app.set('view engine','ejs');
+app.use(expressLayout);
+app.set('layout','./layout/layout')
 
-// to pass cookie to store  data 
-
-// access to main ans  admin
-
-app.use('/',require('./server/routes/main.js'));
-app.use('/',require('./server/routes/admin.js'));
+app.use('/',require('./back/routes/main'));
+const date=mongoose.model('date',{date:Number});
 
 
-const connectDB=require('./server/config/db');
-
-
-
-// to pass data in search bar
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
-
-
-const mongoURI = 'mongodb+srv://pant:tTEBIp7nEP7de3PE@atharv.09g0rli.mongodb.net/'; // Replace with your MongoDB URI
-
-app.use(cookieparser);
-app.use(session({
-    secret:'keyboard cat',
-    resave:false,
-    saveUninitialized:false,
-    store:mongo.create({
-        mongoUrl:mongoURI,
-        collectionName:'sessions'
-    })
-}));
-
-
-
-// Connect to MongoDB
-mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected...'))
-.catch(err => console.log('MongoDB connection error:', err));
-
-// connect to db
-connectDB();
-
-app.listen(5000,async()=>{
-    console.log("running");
-   
-
+mongoose.connect(mongoUrl, {
+  
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.error('Failed to connect to MongoDB', err);
 });
+
+
+
+// Use sessions with MongoDB store
+
+
+
+app.listen(4000,(req,res)=>{
+    console.log('running');
+    });
